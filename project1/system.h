e343fc2523bb6f45af7a4d98a14e868c73f7075b
Oliver Hahn <oliverh@kretskort.uio.no>
2017-02-02 16:42:36 +0100

    changed to be more intuitive

--- project1/system.h
+++ project1/system.h
@@ -8,7 +8,7 @@ using namespace std;
 class system
 {
 public:
-    system(int n_x, int n_y, int s,int c);
+    system(int c);
     vector<int> state;
     int cut, m_nx, m_ny, m_s;
     int pos;
@@ -16,14 +16,12 @@ public:
     void postonum();
     double hermite (int n, double x);
     double wavefunc (double x, double y, double w, double A);
-    void genstate();
     void get (int x, int y, int ms);
     void print();
 
 };
 
 
-/*destruktor*, genstate*/
 
 
 #endif // SYSTEM_H
